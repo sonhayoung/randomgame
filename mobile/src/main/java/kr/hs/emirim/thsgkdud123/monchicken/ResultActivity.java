543520612f02573b00b4dbb42bdd0c49d7e18364
HaYoung Son <thsgkdud123@naver.com>
2016-04-11 20:59:17 +0900

    #8 add splash

--- mobile/src/main/java/kr/hs/emirim/thsgkdud123/monchicken/ResultActivity.java
+++ mobile/src/main/java/kr/hs/emirim/thsgkdud123/monchicken/ResultActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.thsgkdud123.monchicken;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
---
+++ mobile/src/main/java/kr/hs/emirim/thsgkdud123/monchicken/SplashActivity.java
@@ -0,0 +1,29 @@
+package kr.hs.emirim.thsgkdud123.monchicken;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+
+}
